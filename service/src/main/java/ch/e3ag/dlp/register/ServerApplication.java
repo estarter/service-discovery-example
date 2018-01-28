deb7ae06fb22f3bbbe5d5acf6b83f324ca82891b
estarter <estarter@gmail.com>
2018-01-28 18:34:43 +0100

    sample app

--- service/src/main/java/ch/e3ag/dlp/register/ServerApplication.java
+++ service/src/main/java/ch/e3ag/dlp/register/ServerApplication.java
@@ -2,11 +2,13 @@ package ch.e3ag.dlp.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class RegisterApplication {
+@EnableEurekaServer
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegisterApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 }
